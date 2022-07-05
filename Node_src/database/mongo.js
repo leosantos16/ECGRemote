aa9704ddffaf9921a12db8986c01baa322f9eaf2
Leonardo Souza dos Santos <leonardosantos.ch084@academico.ifsul.edu.br>
2022-07-05 19:56:08 -0300

    Mongo atlas

--- Node_src/database/mongo.js
+++ Node_src/database/mongo.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
 const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
-
-let connString = `mongodb://${DB_USER}:${DB_PASS}@db:${DB_PORT}/?authSource=admin`;
-console.log(connString);
+let connString = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/?retryWrites=true&w=majority`;
 
 class dbConnect {
   constructor() {
